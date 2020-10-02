4290b716b7ff1898d44573815819b9a1ca5089ca
mael <mael@bkper.com>
2020-10-02 09:08:16 -0300

    Refactoring buy/sell recordings

--- src/EventHandlerTransactionChecked.ts
+++ src/EventHandlerTransactionChecked.ts
@@ -9,79 +9,104 @@ class EventHandlerTransactionChecked extends EventHandlerTransaction {
     return BotService.flagAccountForRebuildIfNeeded(baseBook, event);
   }
 
-  protected connectedTransactionFound(baseBook: Bkper.Book, connectedBook: Bkper.Book, transaction: bkper.Transaction, connectedTransaction: Bkper.Transaction, stockExcCode: string): string {
-    let bookAnchor = super.buildBookAnchor(connectedBook);
+  protected connectedTransactionFound(financialBook: Bkper.Book, stockBook: Bkper.Book, transaction: bkper.Transaction, connectedTransaction: Bkper.Transaction, stockExcCode: string): string {
+    let bookAnchor = super.buildBookAnchor(stockBook);
     let record = `${connectedTransaction.getDate()} ${connectedTransaction.getAmount()} ${connectedTransaction.getCreditAccountName()} ${connectedTransaction.getDebitAccountName()} ${connectedTransaction.getDescription()}`;
     return `FOUND: ${bookAnchor}: ${record}`;
   }
 
-  protected connectedTransactionNotFound(baseBook: Bkper.Book, connectedBook: Bkper.Book, transaction: bkper.Transaction, stockExcCode: string): string {
-    let baseCreditAccount = baseBook.getAccount(transaction.creditAccount.id);
-    let baseDebitAccount = baseBook.getAccount(transaction.debitAccount.id);
-    let connectedBookAnchor = super.buildBookAnchor(connectedBook);
+  protected connectedTransactionNotFound(financialBook: Bkper.Book, stockBook: Bkper.Book, transaction: bkper.Transaction, stockExcCode: string): string {
+    let financialCreditAccount = financialBook.getAccount(transaction.creditAccount.id);
+    let financialDebitAccount = financialBook.getAccount(transaction.debitAccount.id);
+    let stockBookAnchor = super.buildBookAnchor(stockBook);
 
     let quantity = this.getQuantity(transaction);
     if (quantity == null) {
       return null;
     }
-    let sell = false;
-    let connectedCreditAccountName = this.getConnectedStockAccountName(baseCreditAccount);
-    if (connectedCreditAccountName != null) {
-      sell = true;
-    } else {
-      connectedCreditAccountName = 'Buy';
-    }
 
-    let connectedCreditAccount = connectedBook.getAccount(connectedCreditAccountName);
-    if (connectedCreditAccount == null) {
-        connectedCreditAccount = connectedBook.newAccount().setName(connectedCreditAccountName).setType(sell ? BkperApp.AccountType.ASSET : BkperApp.AccountType.INCOMING).create();
-    }
+    let price = new Number(transaction.amount).valueOf() / quantity;
 
-    let buy = false;
-    let connectedDebitAccountName = this.getConnectedStockAccountName(baseDebitAccount);
-    if (connectedDebitAccountName != null) {
-      buy = true;
-    } else {
-      connectedDebitAccountName = 'Sell';
-    }
-
-    let connectedDebitAccount = connectedBook.getAccount(connectedDebitAccountName);
-    if (connectedDebitAccount == null) {
-        connectedDebitAccount = connectedBook.newAccount().setName(connectedDebitAccountName).setType(buy ? BkperApp.AccountType.ASSET : BkperApp.AccountType.OUTGOING).create();
-    }
-
-    let selling = sell && !buy;
-    let buying = buy && !sell;
-    
-    if (selling || buying) {
-
-      let price = new Number(transaction.amount).valueOf() / quantity;
-
-      let newTransaction = connectedBook.newTransaction()
-      .setDate(transaction.date)
-      .setAmount(quantity)
-      .setCreditAccount(connectedCreditAccount)
-      .setDebitAccount(connectedDebitAccount)
-      .setDescription(transaction.description)
-      .addRemoteId(transaction.id)
-      .setProperty(PRICE_PROP, price.toFixed(baseBook.getFractionDigits()))
-
-      if (buy) {
-        newTransaction.setProperty(ORIGINAL_QUANTITY, quantity.toFixed(0));
+    let stockAccount = this.getConnectedStockAccount(financialBook, stockBook, financialCreditAccount);
+    if (stockAccount) {
+      //Selling
+      let stockSellAccount = stockBook.getAccount(STOCK_SELL_ACCOUNT_NAME);
+      if (stockSellAccount == null) {
+        stockSellAccount = stockBook.newAccount().setName(STOCK_SELL_ACCOUNT_NAME).setType(BkperApp.AccountType.OUTGOING).create();
       }
 
-      let record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${connectedCreditAccount.getName()} ${connectedDebitAccount.getName()} ${newTransaction.getDescription()}`;
-      newTransaction.post();
+      let newTransaction = stockBook.newTransaction()
+      .setDate(transaction.date)
+      .setAmount(quantity)
+      .setCreditAccount(stockAccount)
+      .setDebitAccount(stockSellAccount)
+      .setDescription(transaction.description)
+      .addRemoteId(transaction.id)
+      .setProperty(PRICE_PROP, price.toFixed(financialBook.getFractionDigits()))
+      .post()
 
-      return `POSTED: ${connectedBookAnchor}: ${record}`;
+      let record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${stockAccount.getName()} ${stockSellAccount.getName()} ${newTransaction.getDescription()}`;
+      return `SELL: ${stockBookAnchor}: ${record}`;
+
+    } else {
+      stockAccount = this.getConnectedStockAccount(financialBook, stockBook, financialDebitAccount);
+      if (stockAccount) {
+        //Buying
+        let stockBuyAccount = stockBook.getAccount(STOCK_BUY_ACCOUNT_NAME);
+        if (stockBuyAccount == null) {
+          stockBuyAccount = stockBook.newAccount().setName(STOCK_BUY_ACCOUNT_NAME).setType(BkperApp.AccountType.OUTGOING).create();
+        }        
+
+        let newTransaction = stockBook.newTransaction()
+        .setDate(transaction.date)
+        .setAmount(quantity)
+        .setCreditAccount(stockBuyAccount)
+        .setDebitAccount(stockAccount)
+        .setDescription(transaction.description)
+        .addRemoteId(transaction.id)
+        .setProperty(PRICE_PROP, price.toFixed(financialBook.getFractionDigits()))
+        .setProperty(ORIGINAL_QUANTITY, quantity.toFixed(0))
+        .post()
+
+        let record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${stockBuyAccount.getName()} ${stockAccount.getName()} ${newTransaction.getDescription()}`;
+        return `BUY: ${stockBookAnchor}: ${record}`;
+      }
+
     }
+
+    return null;
 
   }
 
-  private getConnectedStockAccountName(baseAccount: Bkper.Account) {
-    let stockExchangeCode = BotService.getStockExchangeCode(baseAccount);
+  private getConnectedStockAccount(financialBook: Bkper.Book, stockBook: Bkper.Book, financialAccount: Bkper.Account, ): Bkper.Account {
+    let stockExchangeCode = BotService.getStockExchangeCode(financialAccount);
     if (stockExchangeCode != null) {
-      return baseAccount.getName();
+      let stockAccount = stockBook.getAccount(financialAccount.getName());
+      if (stockAccount == null) {
+        stockAccount = stockBook.newAccount()
+          .setName(financialAccount.getName())
+          .setType(financialAccount.getType())
+          .setProperties(financialAccount.getProperties())
+          .setArchived(financialAccount.isArchived());
+        if (financialAccount.getGroups()) {
+          financialAccount.getGroups().forEach(financialGroup => {
+            if (financialGroup) {
+              let stockGroup = stockBook.getGroup(financialGroup.getName());
+              let stockExcCode = financialGroup.getProperty(STOCK_EXC_CODE_PROP);
+              if (stockGroup == null && stockExcCode != null && stockExcCode.trim() != '') {
+                stockGroup = stockBook.newGroup()
+                  .setHidden(financialGroup.isHidden())
+                  .setName(financialGroup.getName())
+                  .setProperties(financialGroup.getProperties())
+                  .create();
+              }
+              stockAccount.addGroup(stockGroup);
+            }
+          });
+        }
+        stockAccount = stockAccount.create();
+      }
+      return stockAccount;
     }
     return null;
   }
