76d282d2629d707f6a36aa82a84eb05c2c17c68b
mael <mael@bkper.com>
2020-10-19 13:49:39 -0300

    Adding order interceptor on restore

--- src/EventHandlerTransactionRestored.ts
+++ src/EventHandlerTransactionRestored.ts
@@ -1,5 +1,9 @@
 class EventHandlerTransactionRestored extends EventHandlerTransaction {
 
+  intercept(baseBook: Bkper.Book, event: bkper.Event): string[] | string {
+    return new InterceptorOrderProcessor().intercept(baseBook, event);
+  }
+
   protected getTransactionQuery(transaction: bkper.Transaction): string {
     return `remoteId:${transaction.id} is:trashed`;
   }
