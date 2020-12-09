8a4cb30568aedaf4b384a3ff4cda096a453243e2
mael <mael@bkper.com>
2020-12-09 09:00:04 -0300

    Fixing fifo ordering algorithm

--- src/RealizedResultsService.ts
+++ src/RealizedResultsService.ts
@@ -256,21 +256,14 @@ namespace RealizedResultsService {
   }
 
   function compareTo(tx1: Bkper.Transaction, tx2: Bkper.Transaction): number {
-    if (tx1.getDateValue() < tx2.getDateValue()) {
-      return -1;
+    if (tx1.getDateValue() != tx2.getDateValue()) {
+      return tx1.getDateValue() - tx2.getDateValue();
     }
-    if (tx1.getProperty(ORDER_PROP) != null || tx2.getProperty(ORDER_PROP) != null) {
+    if (tx1.getProperty(ORDER_PROP) != null && tx2.getProperty(ORDER_PROP) != null) {
       let order1 = +tx1.getProperty(ORDER_PROP);
       let order2 = +tx2.getProperty(ORDER_PROP);
-      if (order1 == null) {
-        order1 = 0;
-      }
-      if (order2 == null) {
-        order2 = 0;
-      }
-      if (order1 < order2) {
-        return -1;
-      }
+      console.log(`${order1} | ${order2}`)
+      return order1 - order2;
     }
     if (tx1.getCreatedAt() && tx2.getCreatedAt()) {
       return tx1.getCreatedAt().getMilliseconds() - tx2.getCreatedAt().getMilliseconds();
