fd3c2a1f626bcb843bad67c23cf011ee04ec4850
victordrumond <victordrumond@hotmail.com>
2022-04-19 16:18:52 -0300

    Remove comments on EventHandlerBookUpdated.ts

--- gcf/src/EventHandlerBookUpdated.ts
+++ gcf/src/EventHandlerBookUpdated.ts
@@ -7,40 +7,15 @@ export class EventHandlerBookUpdated extends EventHandler {
 
     protected async processObject(book: Book, connectedBook: Book, event: bkper.Event): Promise<string> {
 
-        // let connectedCode = getBaseCode(connectedBook);
         let response = '';
         let baseBook = getBaseBook(book);
 
         if (isStockBook(book)) {
-
-            // if (book.getPageSize() != connectedBook.getPageSize()) {
-            //     connectedBook.setPageSize(book.getPageSize())
-            //     response += ` page size: ${book.getPageSize()}`
-            // }
-
-            // if (book.getPeriod() != connectedBook.getPeriod()) {
-            //     connectedBook.setPeriod(book.getPeriod())
-            //     response += ` period: ${book.getPeriod()}`
-            // }
-
-            // if (book.getLockDate() != connectedBook.getLockDate()) {
-            //     connectedBook.setLockDate(book.getLockDate())
-            //     response += ` lock date: ${book.getLockDate()}`
-            // }
-
-            // console.log(book.getPeriodStartMonth())
-
-            // if (book.getPeriodStartMonth() != connectedBook.getPeriodStartMonth()) {
-            //     connectedBook.setPeriodStartMonth(book.getPeriodStartMonth())
-            //     response += ` period start month: ${book.getPeriodStartMonth()}`
-            // }
-
             const stockHistorical = book.getProperty(STOCK_HISTORICAL_PROP);
             if (stockHistorical && stockHistorical != baseBook.getProperty(EXC_HISTORICAL_PROP)) {
                 baseBook.setProperty(EXC_HISTORICAL_PROP, stockHistorical);
                 response += ` ${EXC_HISTORICAL_PROP}: ${stockHistorical}`;
             }
-
         }
 
         if (response != '') {
