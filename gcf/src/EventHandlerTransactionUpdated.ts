b488edabfd2bd38a6ef7ae713ab613904bfadf8f
victordrumond <victordrumond@hotmail.com>
2023-10-16 16:23:53 -0300

    Avoid unnecessary deletion of transactions

--- gcf/src/EventHandlerTransactionUpdated.ts
+++ gcf/src/EventHandlerTransactionUpdated.ts
@@ -8,66 +8,81 @@ import { InterceptorOrderProcessorDeleteFinancial } from "./InterceptorOrderProc
 
 export class EventHandlerTransactionUpdated extends EventHandlerTransaction {
 
-  async intercept(baseBook: Book, event: bkper.Event): Promise<Result> {
-    await new InterceptorOrderProcessorDeleteFinancial().intercept(baseBook, event);
-    return await new InterceptorOrderProcessor().intercept(baseBook, event);
-  }
-
-  protected getTransactionQuery(transaction: bkper.Transaction): string {
-    return `remoteId:${transaction.id}`;
-  }
-
-  protected connectedTransactionNotFound(financialBook: Book, stockBook: Book, financialTransaction: bkper.Transaction, stockExcCode: string): Promise<string> {
-    return null;
-  }
-
-  protected async connectedTransactionFound(financialBook: Book, stockBook: Book, financialTransaction: bkper.Transaction, stockTransaction: Transaction, stockExcCode: string): Promise<string> {
-
-    if (!financialTransaction.posted) {
-      return null;
+    async intercept(baseBook: Book, event: bkper.Event): Promise<Result> {
+        if (this.shouldCascadeDeletion(event)) {
+            await new InterceptorOrderProcessorDeleteFinancial().intercept(baseBook, event);
+        }
+        return await new InterceptorOrderProcessor().intercept(baseBook, event);
     }
 
-    let quantity = this.getQuantity(stockBook, financialTransaction);
-    if (quantity == null || quantity.eq(0)) {
-      return null;
+    private shouldCascadeDeletion(event: bkper.Event): boolean {
+        // No previousAttributes
+        if (!event.data.previousAttributes) {
+            return false;
+        }
+        // No changes OR changed the description only
+        const keys = Object.keys(event.data.previousAttributes);
+        if (keys.length === 0 || (keys.length === 1 && keys[0] === 'description')) {
+            return false;
+        }
+        return true;
     }
 
-    if (stockTransaction.isChecked()) {
-      stockTransaction.uncheck();
+    protected getTransactionQuery(transaction: bkper.Transaction): string {
+        return `remoteId:${transaction.id}`;
     }
 
-    const price = new Amount(financialTransaction.amount).div(quantity);
-    const originalAmount = new Amount(financialTransaction.amount);
-
-    stockTransaction
-      .setDate(financialTransaction.date)
-      .setAmount(quantity)
-      .setDescription(financialTransaction.description)
-      .setProperty(ORIGINAL_QUANTITY_PROP, quantity.toFixed(stockBook.getFractionDigits()))
-      .setProperty(ORIGINAL_AMOUNT_PROP, originalAmount.toString())
-    ;
-
-    if (await isPurchase(stockTransaction)) {
-      stockTransaction.setProperty(PURCHASE_PRICE_PROP, price.toString());
+    protected connectedTransactionNotFound(financialBook: Book, stockBook: Book, financialTransaction: bkper.Transaction, stockExcCode: string): Promise<string> {
+        return null;
     }
 
-    if (await isSale(stockTransaction)) {
-      stockTransaction.setProperty(SALE_PRICE_PROP, price.toString());
+    protected async connectedTransactionFound(financialBook: Book, stockBook: Book, financialTransaction: bkper.Transaction, stockTransaction: Transaction, stockExcCode: string): Promise<string> {
+
+        if (!financialTransaction.posted) {
+            return null;
+        }
+
+        let quantity = this.getQuantity(stockBook, financialTransaction);
+        if (quantity == null || quantity.eq(0)) {
+            return null;
+        }
+
+        if (stockTransaction.isChecked()) {
+            stockTransaction.uncheck();
+        }
+
+        const price = new Amount(financialTransaction.amount).div(quantity);
+        const originalAmount = new Amount(financialTransaction.amount);
+
+        stockTransaction
+            .setDate(financialTransaction.date)
+            .setAmount(quantity)
+            .setDescription(financialTransaction.description)
+            .setProperty(ORIGINAL_QUANTITY_PROP, quantity.toFixed(stockBook.getFractionDigits()))
+            .setProperty(ORIGINAL_AMOUNT_PROP, originalAmount.toString())
+        ;
+
+        if (await isPurchase(stockTransaction)) {
+            stockTransaction.setProperty(PURCHASE_PRICE_PROP, price.toString());
+        }
+
+        if (await isSale(stockTransaction)) {
+            stockTransaction.setProperty(SALE_PRICE_PROP, price.toString());
+        }
+
+        try {
+            await stockTransaction.update();
+        } catch (err) {
+            //Maybe is checked
+            await stockTransaction.uncheck();
+            await stockTransaction.update();
+        }
+
+        await flagStockAccountForRebuildIfNeeded(stockTransaction);
+
+        let bookAnchor = super.buildBookAnchor(stockBook);
+        let record = `EDITED: ${stockTransaction.getDateFormatted()} ${quantity} ${await stockTransaction.getCreditAccountName()} ${await stockTransaction.getDebitAccountName()} ${stockTransaction.getDescription()}`;
+        return `${bookAnchor}: ${record}`;
     }
-
-    try {
-      await stockTransaction.update();
-    } catch (err) {
-      //Maybe is checked
-      await stockTransaction.uncheck();
-      await stockTransaction.update();
-    }
-
-    await flagStockAccountForRebuildIfNeeded(stockTransaction);
-
-    let bookAnchor = super.buildBookAnchor(stockBook);
-    let record = `EDITED: ${stockTransaction.getDateFormatted()} ${quantity} ${await stockTransaction.getCreditAccountName()} ${await stockTransaction.getDebitAccountName()} ${stockTransaction.getDescription()}`;
-    return `${bookAnchor}: ${record}`;
-  }
 
 }
