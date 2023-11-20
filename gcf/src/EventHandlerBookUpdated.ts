5281b7594ede7bd8ac4e6218b8d4ce69a9d7aac1
victordrumond <victordrumond@hotmail.com>
2023-11-20 15:24:38 -0300

    Avoid multiple stock books in the same collection

--- gcf/src/EventHandlerBookUpdated.ts
+++ gcf/src/EventHandlerBookUpdated.ts
@@ -1,5 +1,5 @@
 import { Book } from "bkper";
-import { STOCK_HISTORICAL_PROP, EXC_HISTORICAL_PROP } from "./constants";
+import { STOCK_HISTORICAL_PROP, EXC_HISTORICAL_PROP, STOCK_BOOK_PROP } from "./constants";
 import { EventHandler } from "./EventHandler";
 import { isStockBook, getBaseBook } from "./BotService";
 
@@ -11,10 +11,24 @@ export class EventHandlerBookUpdated extends EventHandler {
         const baseBook = getBaseBook(book);
 
         if (isStockBook(book)) {
+            // stock_historical prop
             const stockHistorical = book.getProperty(STOCK_HISTORICAL_PROP);
             if (stockHistorical != baseBook.getProperty(EXC_HISTORICAL_PROP)) {
                 baseBook.setProperty(EXC_HISTORICAL_PROP, stockHistorical);
                 response += ` ${EXC_HISTORICAL_PROP}: ${stockHistorical}`;
+            }
+        }
+
+        // stock_book prop
+        const stockBookProp = book.getProperty(STOCK_BOOK_PROP);
+        if (stockBookProp) {
+            const collection = book.getCollection();
+            if (collection) {
+                for (const collectionBook of collection.getBooks()) {
+                    if (collectionBook.getProperty(STOCK_BOOK_PROP) && collectionBook.getId() !== book.getId()) {
+                        collectionBook.setProperty(STOCK_BOOK_PROP, '').update();
+                    }
+                }
             }
         }
 
